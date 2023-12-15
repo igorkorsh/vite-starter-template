c847bb24a3c63ecd83fdfb6ab599781b2c8a655e
Igor Korshunov <ygorkorsh@gmail.com>
2023-12-15 15:13:55 +0400

    Оптимизировал скрипт, отвечающий за удаление стилей в форме Маркето

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import "./modules/_navMenu";
 import "./modules/_scrollTo";
 import "./modules/_activeNavLink";
+import "./modules/_deStyleMarketo";
 import "./modules/_addCurrentYear";
 import "./modules/_swiper";
 // import "./modules/_modalWindow";
@@ -10,13 +11,8 @@ import "./modules/_swiper";
 
 import { getScript } from "./modules/_getScript";
 import { addCaptcha } from "./modules/_addCaptcha";
-import { deStyleMarketo } from "./modules/_deStyleMarketo";
 
 if ("MktoForms2" in window) {
-  MktoForms2.whenRendered((form) => {
-    deStyleMarketo(form); // Удаляем стили по умолчанию
-  });
-
   MktoForms2.whenReady((form) => {
     getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback") // Загружаем скрипт динамически
       .then(() => {
--- src/scripts/modules/_deStyleMarketo.js
+++ src/scripts/modules/_deStyleMarketo.js
@@ -1,164 +1,157 @@
-export const deStyleMarketo = (form) => {
-  const formElem = form.getFormElem()[0];
+// Удаляет элементы с указанными классами
+const removeElementsByClass = (formElem, arr) => {
+  const elements = formElem.querySelectorAll(arr);
 
-  const removeElementsByClass = (arr) => {
-    const elements = formElem.querySelectorAll(arr);
+  for (const element of elements) {
+    element.remove();
+  }
+};
 
-    for (const element of elements) {
-      element.remove();
+// Удаляет атрибуты style у элементов формы
+const removeStyleAttributes = (formElem) => {
+  const elements = [...formElem.querySelectorAll("[style]"), formElem];
+
+  for (const element of elements) {
+    element.removeAttribute("style");
+  }
+};
+
+// Отключает таблицы стилей по умолчанию
+const removeStyleSheets = () => {
+  const styleSheets = [...document.styleSheets];
+
+  for (const styles of styleSheets) {
+    if (styles.ownerNode.id.includes("mktoForms2")) {
+      styles.disabled = true;
     }
-  };
 
-  const removeStyleAttributes = () => {
-    const elements = [...formElem.querySelectorAll("[style]"), formElem];
-
-    for (const element of elements) {
-      element.removeAttribute("style");
+    const nextElem = styles.ownerNode.nextElementSibling;
+    if (nextElem && nextElem.tagName === "STYLE") {
+      nextElem.disabled = true;
     }
-  };
+  }
+};
 
-  const removeStyleSheets = () => {
-    const styleSheets = [...document.styleSheets];
+// Удаляет Custom CSS
+const removeCustomStyles = (formElem) => {
+  const tag = formElem.querySelector("style");
 
-    for (const styles of styleSheets) {
-      if (styles.ownerNode.id.includes("mktoForms2")) {
-        styles.disabled = true;
-      }
+  if (tag) {
+    tag.remove();
+  }
+};
 
-      const nextElem = styles.ownerNode.nextElementSibling;
-      if (nextElem && nextElem.tagName === "STYLE") {
-        nextElem.disabled = true;
-      }
+// Если внутри .mktoFormRow есть два элемента .mktoFormCol, то .mktoFormRow становится флекс-контейнером
+const setRowFlexContainer = (formElem) => {
+  const rows = formElem.querySelectorAll(".mktoFormRow");
+
+  for (const row of rows) {
+    const cols = row.querySelectorAll(".mktoFormCol");
+
+    if (cols.length === 2) {
+      row.classList.add("mktoFormFlexRow");
     }
-  };
+  }
+};
 
-  const removeCustomStyles = () => {
-    const tag = formElem.querySelector("style");
+// Исправляет расположение текста относительно type="checkbox" / type="radio"
+const setCheckboxListFlexContainer = (formElem) => {
+  const checkboxes = formElem.querySelectorAll(".mktoCheckboxList");
 
-    if (tag) {
-      tag.remove();
+  for (const checkbox of checkboxes) {
+    checkbox.classList.add("mktoCheckboxListFlexRow");
+  }
+};
+
+// Добавляет текст из label в атрибут placeholder у элементов формы
+const addPlaceholders = (formElem) => {
+  const elements = formElem.querySelectorAll(".mktoField.mktoHasWidth");
+
+  for (const element of elements) {
+    const label = formElem.querySelector(`[for=${element.id}]`);
+
+    if (!element.placeholder) {
+      element.placeholder = label.textContent.replace(/[*:]/g, "");
     }
-  };
+  }
+};
 
-  const setRowFlexContainer = () => {
-    const rows = formElem.querySelectorAll(".mktoFormRow");
+// Добавляет кнопки инкремента/декремента к полю с типом number
+const customInputNumber = (formElem) => {
+  const inputs = formElem.querySelectorAll('input[type="number"]');
 
-    for (const row of rows) {
-      const cols = row.querySelectorAll(".mktoFormCol");
+  const customizeInput = (element) => {
+    const wrapper = element.closest(".mktoFieldWrap");
 
-      if (cols.length === 2) {
-        row.classList.add("mktoFormFlexRow");
-      }
-    }
-  };
+    const increment = () => {
+      const button = document.createElement("button");
+      button.dataset.value = "up";
 
-  const setCheckboxListFlexContainer = () => {
-    const checkboxes = formElem.querySelectorAll(".mktoCheckboxList");
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    for (const checkbox of checkboxes) {
-      checkbox.classList.add("mktoCheckboxListFlexRow");
-    }
-  };
+        const oldValue = parseFloat(element.value);
+        const maxValue = element.max ? parseFloat(element.max) : Number.MAX_SAFE_INTEGER;
+        let newValue;
 
-  const addPlaceholders = () => {
-    const elements = formElem.querySelectorAll(".mktoField.mktoHasWidth");
+        if (isNaN(oldValue)) {
+          newValue = 1;
+        } else if (oldValue >= maxValue) {
+          newValue = maxValue;
+        } else {
+          newValue = oldValue + 1;
+        }
+        element.value = newValue;
+      });
 
-    for (const element of elements) {
-      const label = formElem.querySelector(`[for=${element.id}]`);
+      return button;
+    };
+    const decrement = () => {
+      const button = document.createElement("button");
+      button.dataset.value = "down";
 
-      if (!element.placeholder) {
-        element.placeholder = label.textContent.replace(/[*:]/g, "");
-      }
-    }
-  };
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
 
-  const customInputNumber = () => {
-    const inputs = formElem.querySelectorAll('input[type="number"]');
+        const oldValue = parseFloat(element.value);
+        const minValue = element.min ? parseFloat(element.min) : 0;
+        let newValue;
 
-    const customizeInput = (element) => {
-      const wrapper = element.closest(".mktoFieldWrap");
+        if (isNaN(oldValue)) {
+          newValue = minValue;
+        } else if (oldValue <= minValue) {
+          newValue = minValue;
+        } else {
+          newValue = oldValue - 1;
+        }
+        element.value = newValue;
+      });
 
-      const increment = () => {
-        const button = document.createElement("button");
-        button.dataset.value = "up";
-
-        button.addEventListener("click", (event) => {
-          event.preventDefault();
-
-          const oldValue = parseFloat(element.value);
-          const maxValue = element.max ? parseFloat(element.max) : Number.MAX_SAFE_INTEGER;
-          let newValue;
-
-          if (isNaN(oldValue)) {
-            newValue = 1;
-          } else if (oldValue >= maxValue) {
-            newValue = maxValue;
-          } else {
-            newValue = oldValue + 1;
-          }
-          element.value = newValue;
-        });
-
-        return button;
-      };
-      const decrement = () => {
-        const button = document.createElement("button");
-        button.dataset.value = "down";
-
-        button.addEventListener("click", (event) => {
-          event.preventDefault();
-
-          const oldValue = parseFloat(element.value);
-          const minValue = element.min ? parseFloat(element.min) : 0;
-          let newValue;
-
-          if (isNaN(oldValue)) {
-            newValue = minValue;
-          } else if (oldValue <= minValue) {
-            newValue = minValue;
-          } else {
-            newValue = oldValue - 1;
-          }
-          element.value = newValue;
-        });
-
-        return button;
-      };
-
-      wrapper.classList.add("mktoCustomNumberField");
-      wrapper.appendChild(increment());
-      wrapper.appendChild(decrement());
+      return button;
     };
 
-    for (const input of inputs) {
-      customizeInput(input);
-    }
+    wrapper.classList.add("mktoCustomNumberField");
+    wrapper.appendChild(increment());
+    wrapper.appendChild(decrement());
   };
 
-  //
-  // Инициализация
-  //
-  // Удаляет элементы с указанными классами
-  removeElementsByClass([".mktoAsterix", ".mktoClear", ".mktoGutter", ".mktoOffset"]);
-
-  // Удаляет атрибуты style у элементов формы
-  removeStyleAttributes();
-
-  // Отключает таблицы стилей по умолчанию
-  removeStyleSheets();
-
-  // Удаляет Custom CSS
-  removeCustomStyles();
-
-  // Если внутри .mktoFormRow есть два элемента .mktoFormCol, то .mktoFormRow становится флекс-контейнером
-  setRowFlexContainer();
-
-  // Исправляет расположение текста относительно type="checkbox" / type="radio"
-  setCheckboxListFlexContainer();
-
-  // Добавляет текст из label в атрибут placeholder у элементов формы
-  addPlaceholders();
-
-  // Добавляет кнопки инкремента/декремента к полю с типом number
-  customInputNumber();
+  for (const input of inputs) {
+    customizeInput(input);
+  }
 };
+
+if ("MktoForms2" in window) {
+  MktoForms2.whenRendered((form) => {
+    const formElem = form.getFormElem()[0];
+    const classNames = [".mktoAsterix", ".mktoClear", ".mktoGutter", ".mktoOffset"];
+
+    removeElementsByClass(formElem, classNames);
+    removeStyleAttributes(formElem);
+    removeStyleSheets();
+    removeCustomStyles(formElem);
+    setRowFlexContainer(formElem);
+    setCheckboxListFlexContainer(formElem);
+    addPlaceholders(formElem);
+    customInputNumber(formElem);
+  });
+}
